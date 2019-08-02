4ccd1c83db61dfe9e954cab1b62db506969ee167
amirthaa <47654292+amirthaa@users.noreply.github.com>
2019-08-02 14:52:03 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Prime
+{
+ int flag;
+ Scanner scan=new Scanner(System.in);
+ int n=scan.nextInt();
+ {
+   if(n%i==0)
+   flag=1;
+ }
+  if(flag==0)
+  Sysytem.out.println("prime");
+  else
+  Sysytem.out.println("not prime");
+}
